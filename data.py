c1df17c8178a1c5c57359d512e21e4800f4b3c74
katyamescheryakova <146254268+katyamescheryakova@users.noreply.github.com>
2023-09-27 22:12:41 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,20 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_body = {
+    "firstName": "Александр",
+    "lastName": "Пушкин",
+    "address": "Полянка, 38",
+    "metroStation": 204,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 3,
+    "deliveryDate": "2023-09-26",
+    "comment": "Я Вас любил!",
+    "color": [
+        "BLACK"
+    ]
+}
+track_number = {
+        'track': 634973,
+    }
