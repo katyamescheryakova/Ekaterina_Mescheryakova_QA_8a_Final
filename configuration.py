51f9bd4932f9f1c2432391ec96cc4e616cef0386
katyamescheryakova <146254268+katyamescheryakova@users.noreply.github.com>
2023-09-27 22:11:19 +0300

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://2693d729-c504-466e-9e40-d56c35ecdc81.serverhub.praktikum-services.ru" #Адрес сервера
+CREATE_ORDER_PATH = "/api/v1/orders"  #Ручка на создание заказа
+GET_ORDER_PATH = "/api/v1/orders/track"  #Ручка на получение трек-номера заказа
+DOC_PATH = "/docs/"  #Документация API
